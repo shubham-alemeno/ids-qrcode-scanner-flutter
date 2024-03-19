ea7a73fe75df2790d6afdee62c77be4e2c4a5eb0
pnaskardev <pnaskardev@gmail.com>
2024-03-19 12:56:29 +0530

    refactored some code

--- lib/features/app/app_builder.dart
+++ lib/features/app/app_builder.dart
@@ -2,24 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ids_qrcode_scanner/features/home/bloc/network_bloc.dart';
 import 'package:ids_qrcode_scanner/features/home/presentation/home_page.dart';
-import 'package:ids_qrcode_scanner/features/qrscanner/cubit/scannerdata_cubit.dart';
 
 class AppBuilder extends StatelessWidget {
   const AppBuilder({super.key});
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return MultiBlocProvider(
-  //     providers: [
-  //       BlocProvider(create: (context) => NetworkBloc()..add(NetworkObserve()),),
-  //       BlocProvider(create: (_) => ScannerDataCubit(),)
-  //     ],
-  //     child: const MaterialApp(
-  //       home: HomePage(),
-  //     ),
-  //   );
-  // }
-
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/features/qrscanner/presentation/qr_scanner_page.dart
+++ lib/features/qrscanner/presentation/qr_scanner_page.dart
@@ -1,6 +1,5 @@
 import 'dart:developer';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ids_qrcode_scanner/features/qrscanner/cubit/scannerdata_cubit.dart';
