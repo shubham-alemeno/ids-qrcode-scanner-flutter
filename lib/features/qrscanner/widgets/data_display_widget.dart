8f3fe5da6f8dfe81d44cb38929ceefec3043c88c
pnaskardev <pnaskardev@gmail.com>
2024-03-20 16:41:00 +0530

    Refactor data display in DataDisplayWidget

--- lib/features/qrscanner/widgets/data_display_widget.dart
+++ lib/features/qrscanner/widgets/data_display_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:ids_qrcode_scanner/features/qrscanner/data/models.dart';
 
 Widget DataDisplayWidget({required Appointment listItem}) {
@@ -13,99 +12,92 @@ Widget DataDisplayWidget({required Appointment listItem}) {
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("textJaOsh: "), Text(listItem.textJaOsh)],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [const Text("customerEmail: "), Text(listItem.customerEmail)],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("customerFirstName: "), Text(listItem.customerFirstName)],
+                children: [const Text("Customer First Name: "), Text(listItem.customerFirstName)],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("customerFullName: "), Text(listItem.customerFullName)],
+                children: [const Text("Customer Last Name: "), Text(listItem.customerLastName)],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("customerLastName: "), Text(listItem.customerLastName)],
+                children: [const Text("Customer Phone no: "), Text(listItem.customerPhone)],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("customerPhone: "), Text(listItem.customerPhone)],
+                children: [const Text("Customer Note: "), Text(listItem.customerNote)],
+              ),
+              // Line Break-------------------------->
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [const Text("Service Name: "), Text(listItem.serviceName)],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("customerNote: "), Text(listItem.customerNote)],
-              ),
-              Wrap(
-                children: [const Text("customerCancelAppointmentLink: "), Text(listItem.customerCancelAppointmentLink)],
+                children: [const Text("Appointment Status: "), Text(listItem.appointmentStatus)],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("serviceExtras: "), Text(listItem.serviceExtras)],
+                children: [const Text("Booking ID: "), Text(listItem.bookingId)],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("serviceName: "), Text(listItem.serviceName)],
+                children: [const Text("Appointment Date: "), Text(listItem.appointmentDate)],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("serviceAmount: "), Text(listItem.serviceAmount)],
+                children: [const Text("Appointment Time: "), Text(listItem.appointmentTime)],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("serviceDuration: "), Text(listItem.serviceDuration)],
+                children: [const Text("No. of Persons: "), Text(listItem.numberOfPerson)],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("categoryName: "), Text(listItem.categoryName)],
+                children: [const Text("Service Extras: "), Text(listItem.serviceExtras)],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("appointmentDate: "), Text(listItem.appointmentDate)],
+                children: [const Text("Service Amount: "), Text(listItem.serviceAmount)],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("appointmentTime: "), Text(listItem.appointmentTime)],
+                children: [const Text("Tax Amount: "), Text(listItem.taxAmount)],
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("appointmentDateTime: "), Text(listItem.appointmentDateTime)],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("appointmentDuration: "), Text(listItem.appointmentDuration)],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("appointmentStartTime: "), Text(listItem.appointmentStartTime)],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("appointmentEndTime: "), Text(listItem.appointmentEndTime)],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("taxAmount: "), Text(listItem.taxAmount)],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("numberOfPerson: "), Text(listItem.numberOfPerson)],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("discountAmount: "), Text(listItem.discountAmount)],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("appointmentStatus: "), Text(listItem.appointmentStatus)],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Text("bookingId: "), Text(listItem.bookingId)],
-              ),
+              // Wrap(
+              //   children: [const Text("customerCancelAppointmentLink: "), Text(listItem.customerCancelAppointmentLink)],
+              // ),
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //   children: [const Text("serviceDuration: "), Text(listItem.serviceDuration)],
+              // ),
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //   children: [const Text("categoryName: "), Text(listItem.categoryName)],
+              // ),
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //   children: [const Text("appointmentDateTime: "), Text(listItem.appointmentDateTime)],
+              // ),
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //   children: [const Text("appointmentDuration: "), Text(listItem.appointmentDuration)],
+              // ),
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //   children: [const Text("appointmentStartTime: "), Text(listItem.appointmentStartTime)],
+              // ),
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //   children: [const Text("appointmentEndTime: "), Text(listItem.appointmentEndTime)],
+              // ),
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //   children: [const Text("discountAmount: "), Text(listItem.discountAmount)],
+              // ),
             ],
           ),
         ),
